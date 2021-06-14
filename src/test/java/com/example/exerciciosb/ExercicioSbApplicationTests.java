87558eaa2457551de31c62a85021e91d7474f18f
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-06-14 19:14:30 -0300

    Arquivo de Teste

---
+++ src/test/java/com/example/exerciciosb/ExercicioSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exerciciosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
