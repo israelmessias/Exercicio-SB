448cabe3a19da304a61bee9756dcf0971bc43ab6
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-06-14 19:19:53 -0300

    Arquivo Main do projeto Spring Boot

---
+++ src/main/java/com/example/exerciciosb/ExercicioSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.exerciciosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExercicioSbApplication.class, args);
+    }
+
+}
